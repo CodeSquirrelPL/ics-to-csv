ac8845b42763f972273bc86e9b5b04f1ec8b6b2b
Nikita Filipiak <33207529+CodeSquirrelPL@users.noreply.github.com>
2023-01-14 15:57:57 +0100

    added files: input, notes and script

---
+++ script.py
@@ -0,0 +1,18 @@
+outputProperties = ['DTSTART', 'SUMMARY', 'DESCRIPTION', 'URL']
+
+print('hello world')
+#File_object = open("frankfurt2023.ics","r");
+output = ""
+
+#with open("frankfurt2023.ics","r") as inputFile:
+#    for line in inputFile:
+#        output += line.strip()
+
+
+for x in range (5):
+    print(x)
+    print(str(x),' m')
+
+
+
+
