41c784c87185cef74ff9cd9fa47c404f2efa384b
CodeSquirrelPL <33207529+CodeSquirrelPL@users.noreply.github.com>
2023-01-15 09:47:18 +0100

    refactor: change names to comply with PEP 8

--- script.py
+++ script.py
@@ -2,37 +2,38 @@ import re
 import html
 from datetime import datetime
 
-openingKeyword = 'BEGIN:VEVENT'
-closingKeyword = 'END:VEVENT'
-desiredAttributes = ['DTSTART', 'SUMMARY', 'DESCRIPTION', 'URL']
-outputFileName = 'calendarAs.csv'
-inputFilePath = 'frankfurt2023.ics'
+OPENING_KEYWORD = 'BEGIN:VEVENT'
+CLOSING_KEYWORD = 'END:VEVENT'
+DESIRED_ATTRIBUTES = ['DTSTART', 'SUMMARY', 'DESCRIPTION', 'URL']
+OUTPUT_FILE_PATH = 'calendarAs.csv'
+INPUT_FILE_PATH = 'frankfurt2023.ics'
+REDUNDANT_EVENTS = ['Full Moon', 'Moon at Last Quarter', 'Moon at First Quarter']
 
-def RemoveUrl(description):
+def remove_url(description):
     regex = '\.\shttps:\/\/.+'
     return re.sub(regex, '', description)
 
-def LineIntoAttribute(inputLine):
-    [attributeName, attributeValue] = re.split(':', inputLine, 1)
-    if attributeName in desiredAttributes:
-        if (attributeName=='DESCRIPTION'):
-            attributeValue = RemoveUrl(attributeValue)
-            attributeValue = html.unescape(attributeValue)
-        if (attributeName=='DTSTART'):
-            attributeValue = convertTime(attributeValue)
-        return [attributeName, attributeValue]
+def line_into_attribute(input_line):
+    [attribute_name, attribute_value] = re.split(':', input_line, 1)
+    if attribute_name in DESIRED_ATTRIBUTES:
+        if (attribute_name=='DESCRIPTION'):
+            attribute_value = remove_url(attribute_value)
+            attribute_value = html.unescape(attribute_value)
+        if (attribute_name=='DTSTART'):
+            attribute_value = time_from_ical(attribute_value)
+        return [attribute_name, attribute_value]
     else:
         return [False, False]
 
-def LinesIntoEvent(lines):
+def lines_into_event(lines):
     event = {}
     for line in lines:
-        [attributeName, attributeValue] = LineIntoAttribute(line)
-        if (attributeName and attributeValue):
-            event[attributeName] = attributeValue
+        [attribute_name, attribute_value] = line_into_attribute(line)
+        if (attribute_name and attribute_value):
+            event[attribute_name] = attribute_value
     return event
 
-def itemsListIntoCsvText(items):
+def items_list_into_csv_text(items):
     output = ""
     for item in items:
         for attribute in item.values():
@@ -40,45 +41,36 @@ def itemsListIntoCsvText(items):
         output += '\n'
     return output
 
-def saveAsCsv(text):
-    outputFile = open(outputFileName, 'w', encoding='utf8')
-    outputFile.write(text)
+def save_as_csv(text):
+    output_file = open(OUTPUT_FILE_PATH, 'w', encoding='utf8')
+    output_file.write(text)
 
-def convertTime(iCalTime):
+def time_from_ical(iCalTime):
     output = datetime.strptime(iCalTime, "%Y%m%dT%H%M%SZ")
     return output.isoformat()
 
+all_events = []
 
-allEvents = []
+input_file = open(INPUT_FILE_PATH, 'r', encoding='utf8')
 
-inputFile = open(inputFilePath, 'r', encoding='utf8')
-
-count = 0
-
-line = inputFile.readline()
-newLine = re.sub('\n', '', line)
-#print('"'+newLine+'"')
-if (newLine == 'BEGIN:VCALENDAR'):
-    print('file start')
-
-for line in inputFile:
+for line in input_file:
     line = re.sub('\n', '', line)
-    if (line == openingKeyword):
-        allEventAttributes = []
-        for eventDescriptionLine in inputFile:
-            eventDescriptionLine = re.sub('\n', '', eventDescriptionLine)
-            if (eventDescriptionLine!= closingKeyword):
-                allEventAttributes.append(eventDescriptionLine)
+    if (line == OPENING_KEYWORD):
+        all_event_attributes = []
+        for event_description_line in input_file:
+            event_description_line = re.sub('\n', '', event_description_line)
+            if (event_description_line!= CLOSING_KEYWORD):
+                all_event_attributes.append(event_description_line)
             else:
-                event = LinesIntoEvent(allEventAttributes)
-                allEvents.append(event)
+                event = lines_into_event(all_event_attributes)
+                all_events.append(event)
                 break
         
 
-inputFile.close()
+input_file.close()
 
-outputText = itemsListIntoCsvText(allEvents)
+output_text = items_list_into_csv_text(all_events)
 
-saveAsCsv(outputText)
+save_as_csv(output_text)
 
 
