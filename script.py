a662aa88b22ead6c121bf049b9736cad9f0cee00
CodeSquirrelPL <33207529+CodeSquirrelPL@users.noreply.github.com>
2023-01-15 02:30:51 +0100

    finish main feature - convert data from ics file to csv

--- script.py
+++ script.py
@@ -1,18 +1,84 @@
-outputProperties = ['DTSTART', 'SUMMARY', 'DESCRIPTION', 'URL']
+import re
+import html
+from datetime import datetime
 
-print('hello world')
-#File_object = open("frankfurt2023.ics","r");
-output = ""
+openingKeyword = 'BEGIN:VEVENT'
+closingKeyword = 'END:VEVENT'
+desiredAttributes = ['DTSTART', 'SUMMARY', 'DESCRIPTION', 'URL']
+outputFileName = 'calendarAs.csv'
+inputFilePath = 'frankfurt2023.ics'
 
-#with open("frankfurt2023.ics","r") as inputFile:
-#    for line in inputFile:
-#        output += line.strip()
+def RemoveUrl(description):
+    regex = '\.\shttps:\/\/.+'
+    return re.sub(regex, '', description)
+
+def LineIntoAttribute(inputLine):
+    [attributeName, attributeValue] = re.split(':', inputLine, 1)
+    if attributeName in desiredAttributes:
+        if (attributeName=='DESCRIPTION'):
+            attributeValue = RemoveUrl(attributeValue)
+            attributeValue = html.unescape(attributeValue)
+        if (attributeName=='DTSTART'):
+            attributeValue = convertTime(attributeValue)
+        return [attributeName, attributeValue]
+    else:
+        return [False, False]
+
+def LinesIntoEvent(lines):
+    event = {}
+    for line in lines:
+        [attributeName, attributeValue] = LineIntoAttribute(line)
+        if (attributeName and attributeValue):
+            event[attributeName] = attributeValue
+    return event
+
+def itemsListIntoCsvText(items):
+    output = ""
+    for item in items:
+        for attribute in item.values():
+            output = output + '"' + attribute + '",'
+        output += '\n'
+    return output
+
+def saveAsCsv(text):
+    outputFile = open(outputFileName, 'w', encoding='utf8')
+    outputFile.write(text)
+
+def convertTime(iCalTime):
+    output = datetime.strptime(iCalTime, "%Y%m%dT%H%M%SZ")
+    return output.isoformat()
 
 
-for x in range (5):
-    print(x)
-    print(str(x),' m')
+allEvents = []
 
+inputFile = open(inputFilePath, 'r', encoding='utf8')
 
+count = 0
+
+line = inputFile.readline()
+newLine = re.sub('\n', '', line)
+#print('"'+newLine+'"')
+if (newLine == 'BEGIN:VCALENDAR'):
+    print('file start')
+
+for line in inputFile:
+    line = re.sub('\n', '', line)
+    if (line == openingKeyword):
+        allEventAttributes = []
+        for eventDescriptionLine in inputFile:
+            eventDescriptionLine = re.sub('\n', '', eventDescriptionLine)
+            if (eventDescriptionLine!= closingKeyword):
+                allEventAttributes.append(eventDescriptionLine)
+            else:
+                event = LinesIntoEvent(allEventAttributes)
+                allEvents.append(event)
+                break
+        
+
+inputFile.close()
+
+outputText = itemsListIntoCsvText(allEvents)
+
+saveAsCsv(outputText)
 
 
